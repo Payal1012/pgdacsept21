7c56a29d4e54d96e9755f3c5dba4221f4075e1ef
Payal <payalparhe79@gmail.com>
2021-10-06 14:57:26 +0530

    first copy of my file.

---
+++ add1.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+void addtwo()
+{
+    int num1,num2;
+    printf("Enter two numberS:  " );
+    scanf("%d%d", &num1, &num2);
+    printf("Sum of addtion is = %d\n",num1+num2);
+
+}
+
+void main()
+{
+
+}
