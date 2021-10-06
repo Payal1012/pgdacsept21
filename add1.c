7d86a8d1bbe4d71a0564908c93f9fc8cfadf45d1
Payal1012 <91121483+Payal1012@users.noreply.github.com>
2021-10-06 16:59:04 +0530

    Add files via upload

---
+++ add1.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+void addtwo()
+{
+    int num1,num2;
+    printf("Enter two numberS:  " );
+    scanf("%d%d", &num1, &num2);
+    printf("Sum of %d is %d = %d\n",num1,num2,num1+num2);
+
+}
+
+void main()
+{
+	addtwo();
+}
